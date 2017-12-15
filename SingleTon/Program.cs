c147df6e624229319c484da5569aa6557af1ce72
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-12-15 10:49:24 +0600

    Implement SingleTon with minimum code

---
+++ SingleTon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * We can not create instance of SingleTon
+            SingleTon singleTon = new SingleTon();
+            */
+            var singleTon = SingleTon.Instanse;
+        }
+    }
+}
---
+++ SingleTon/SingleTon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    // Sealed is used to privent calling constractor from inner classes
+    public sealed class SingleTon
+    {
+        private static SingleTon instanse;
+        public static SingleTon Instanse
+        {
+            // If instance is already available return the instance otherwise return new one
+            get
+            {
+                if (instanse == null)
+                    return new SingleTon();
+                return instanse;
+            }
+        }
+        // Constractor is private so it is not possible 
+        // to create a new instance of the object
+        private SingleTon() { }
+    }
+}
